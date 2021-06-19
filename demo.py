f1b1e194785c1322054dad9d7e2ed6b9b36365c3
Simon Varga <simon.varga.2008@gmail.com>
2021-06-19 13:46:45 +0200

    my first steps

---
+++ demo.py
@@ -0,0 +1,23 @@
+sum = 0
+for i in range ( 0 ):
+    sum += i
+    print ( sum )
+
+x = 5
+y = "Nieco"
+print( x, y )
+
+print2 = 22
+print( print2 )
+
+def print3(item):
+    print(item)
+    print(item)
+
+print3( 33 )
+print()
+
+def x2( x ):
+    print( x * x )
+
+x2( 5 )
